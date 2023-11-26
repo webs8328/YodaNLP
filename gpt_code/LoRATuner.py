1e8fff904d4dcbc157188a06a21cea64525eb9a2
Kaushik Vemparala <kaushikny13@gmail.com>
2023-11-26 17:23:13 -0500

    edits

--- gpt_code/LoRATuner.py
+++ gpt_code/LoRATuner.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow_datasets as tfds
 from peft import LoraConfig, get_peft_model, peft_model
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, TextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments, EvalPrediction
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, TextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments, EvalPrediction, pipeline
 import os
 import numpy as np
 from sklearn.metrics import log_loss
@@ -16,7 +16,6 @@ class LoRATuner:
     def __init__(self, model_name) -> None:
         self.base_model = GPT2LMHeadModel.from_pretrained(model_name)
         self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-        self.tuned_model = None
     
     def print_num_trainable_features(self):
         """
@@ -48,7 +47,7 @@ class LoRATuner:
         return {"perplexity": perplexity}
 
     #Citation: Got this function template from https://212digital.medium.com/fine-tuning-the-gpt-2-large-language-model-unlocking-its-full-potential-66e3a082ab9c
-    def tune(self, train_file_name, val_file_name, model_id, config, eval_steps=500, save_steps = 500, epochs = 1, max_steps = -1):
+    def tune(self, train_file_name, val_file_name, model_id, config, eval_steps=500, logging_steps=500, save_steps = 500, epochs = 1, max_steps = -1):
         train_file = os.path.join("../data/", train_file_name)
         val_file = os.path.join("../data/", val_file_name)
         lora_model = get_peft_model(self.base_model, config)
@@ -70,18 +69,22 @@ class LoRATuner:
             num_train_epochs=epochs,
             per_device_train_batch_size=4,
             save_steps=save_steps,
+            save_strategy="steps",
             evaluation_strategy="steps", 
             do_eval=True,
             eval_steps=eval_steps, 
-            max_steps=max_steps, 
-            eval_accumulation_steps=15
+            logging_dir='./logs',
+            logging_steps=logging_steps,
+            max_steps=max_steps,
+            prediction_loss_only=True,
+            do_predict=True,
+            eval_accumulation_steps=10
         )
         trainer = Trainer(
             model=lora_model,
             args=training_args,
             data_collator=data_collator,
             train_dataset=train_dataset,
-            compute_metrics=self.compute_metrics,
             eval_dataset=val_dataset,
             tokenizer=self.tokenizer
         )
@@ -89,6 +92,11 @@ class LoRATuner:
         
         self.tuned_model = lora_model
         lora_model.save_pretrained(model_id)
+
+    def create_pipeline(checkpoint_path):
+        tokenizer = GPT2Tokenizer.from_pretrained(checkpoint_path)
+        model = GPT2LMHeadModel.from_pretrained(checkpoint_path)
+        return pipeline('text-generation', model=model, tokenizer = tokenizer)
 
     def get_response(pipeline, prompt, checkpoint_path):
         tokenizer = GPT2Tokenizer.from_pretrained(checkpoint_path)
