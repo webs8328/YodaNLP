19b31e6765af06ed6f25db77f1ae59def487cbba
Jonathon Webster <webs8328@berkeley.edu>
2023-11-27 15:56:59 -0800

    substantial changes

--- gpt_code/LoRATuner.py
+++ gpt_code/LoRATuner.py
@@ -1,7 +1,5 @@
 from datasets import load_dataset
 import matplotlib.pyplot as plt
-#import tensorflow as tf
-#import tensorflow_datasets as tfds
 from peft import LoraConfig, get_peft_model, peft_model
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, TextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments, EvalPrediction, EarlyStoppingCallback
 import os
@@ -14,6 +12,9 @@ from scipy.special import softmax
 from sklearn.metrics import log_loss
 import sklearn
 import torch
+from tqdm import tqdm
+from evaluate import load
+import random
 
 
 class LoRATuner:
@@ -40,23 +41,10 @@ class LoRATuner:
             f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param:.2f}"
         )
 
-    #Citation, got this from: https://212digital.medium.com/evaluating-gpt-2-language-model-a-step-by-step-guide-b451339e6a41
-    def compute_metrics(self, p):
-        logits = p.predictions
-        logits = logits / np.linalg.norm(logits, axis=-1, keepdims=True)
-        labels = p.label_ids
-        probabilities = softmax(logits, axis=-1)
-        loss = log_loss(labels.flatten(), probabilities.reshape(-1, probabilities.shape[-1]), labels=[i for i in range(logits.shape[-1])])
-        perplexity = np.exp(loss)
-        return {"perplexity": perplexity}
-
-
     #Citation: Got this function template from https://212digital.medium.com/fine-tuning-the-gpt-2-large-language-model-unlocking-its-full-potential-66e3a082ab9c
     def tune(self, train_file_name, val_file_name, model_id, config, eval_steps=500, logging_steps=500, save_steps = 500, epochs = 1, max_steps = -1, seed = 0):
         train_file = os.path.join("../data/", train_file_name)
         val_file = os.path.join("../data/", val_file_name)
-        print(val_file)
-        print(train_file)
         set_seed(seed)
         lora_model = get_peft_model(self.base_model, config)
         
@@ -69,7 +57,6 @@ class LoRATuner:
             tokenizer=self.tokenizer,
             file_path=val_file,
             block_size=128)
-        print(val_dataset)
 
         print(os.path.join('./logs/', model_id))
         
@@ -101,18 +88,44 @@ class LoRATuner:
             train_dataset=train_dataset,
             eval_dataset=val_dataset,
             tokenizer=self.tokenizer, 
-            #compute_metrics=self.compute_metrics,
             callbacks=[EarlyStoppingCallback(early_stopping_patience = 2)]
         )
         trainer.train()
         self.tuned_model = lora_model
         lora_model.save_pretrained(model_id)
 
-    def create_pipeline(checkpoint_path):
-        tokenizer = GPT2Tokenizer.from_pretrained(checkpoint_path)
-        model = GPT2LMHeadModel.from_pretrained(checkpoint_path)
-        return pipeline('text-generation', model=model, tokenizer = tokenizer)
+def create_pipeline(checkpoint_path):
+    tokenizer = GPT2Tokenizer.from_pretrained(checkpoint_path)
+    model = GPT2LMHeadModel.from_pretrained(checkpoint_path)
+    return pipeline('text-generation', model=model, tokenizer = tokenizer)
+def get_response(pipeline, prompt, seed):
+    set_seed(seed)
+    return pipeline(prompt)[0]['generated_text']
+def get_perplexity(checkpoints , test_data_file, path, seed):
+    perplexity = load("perplexity", module_type="metric")
+    avg_perps = []
+    test_file = os.path.join("../data/", test_data_file)
+    with open(test_file, 'r') as file:
+        data = file.read()
+    data_list = data.split("\n")
+    random.seed(seed)
+    random_seeds = []
+    for _ in range(len(data_list)):
+        random_seeds.append(random.randint(0, 3000000))
+    for i in checkpoints:
+        _path = path + "/checkpoint-" + str(i)
+        pipe = create_pipeline(_path)
+        outputs = []
+        for j in range(len(data_list)):
+            outputs.append(get_response(pipe, data_list[j], random_seeds[j]))
+        avg_perps.append((i, perplexity.compute(predictions=outputs, model_id=_path)['mean_perplexity']))
+    return avg_perps
 
-    def get_response(pipeline, prompt, seed):
-        set_seed(seed)
-        return pipeline(prompt)[0]['generated_text']
+        
+        
+
+
+
+
+
+    
